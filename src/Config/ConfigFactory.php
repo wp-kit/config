aca9108d2c7eb113205b1e91e6f4bd284db504a7
terence1990 <terence@creativelittledots.co.uk>
2017-08-22 22:09:49 +0100

    Improved Factory

--- src/Config/ConfigFactory.php
+++ src/Config/ConfigFactory.php
@@ -2,11 +2,9 @@
 
 namespace WPKit\Config;
 
-use WPKit\Finder\Finder;
-use Illuminate\Support\Arr;
-use Illuminate\Contracts\Config\Repository;
+use Illuminate\Config\Repository;
 
-class ConfigFactory implements IConfig, Repository, \ArrayAccess
+class ConfigFactory extends Repository implements IConfig
 {
     /**
      * Config file finder instance.
@@ -14,28 +12,10 @@ class ConfigFactory implements IConfig, Repository, \ArrayAccess
      * @var ConfigFinder
      */
     protected $finder;
-    
-    /**
-     * All of the configuration items.
-     *
-     * @var array
-     */
-    protected $items = [];
 
     public function __construct(ConfigFinder $finder)
     {
         $this->finder = $finder;
-    }
-    
-    /**
-     * Determine if the given configuration value exists.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function has($key)
-    {
-        return Arr::has($this->items, $key);
     }
 
     /**
@@ -57,66 +37,9 @@ class ConfigFactory implements IConfig, Repository, \ArrayAccess
 		    $this->set($file, $properties);
 	        
 		}
+		
+		return parent::get($key, $default);
 
-        return Arr::get($this->items, $key, $default);
-    }
-    
-    /**
-     * Set a given configuration value.
-     *
-     * @param  array|string  $key
-     * @param  mixed   $value
-     * @return void
-     */
-    public function set($key, $value = null)
-    {
-        $keys = is_array($key) ? $key : [$key => $value];
-        foreach ($keys as $key => $value) {
-            Arr::set($this->items, $key, $value);
-        }
-    }
-    
-    /**
-     * Determine if the given configuration option exists.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function offsetExists($key)
-    {
-        return $this->has($key);
-    }
-    /**
-     * Get a configuration option.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function offsetGet($key)
-    {
-        return $this->get($key);
-    }
-    /**
-     * Set a configuration option.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return void
-     */
-    public function offsetSet($key, $value)
-    {
-        $this->set($key, $value);
-    }
-    
-    /**
-     * Unset a configuration option.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    public function offsetUnset($key)
-    {
-        $this->set($key, null);
     }
     
 }
