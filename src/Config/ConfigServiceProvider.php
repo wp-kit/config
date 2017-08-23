dfa9cb1f11d37c9d274badb00813d589b76b3975
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-08-23 16:49:19 +0100

    Update ConfigServiceProvider.php

--- src/Config/ConfigServiceProvider.php
+++ src/Config/ConfigServiceProvider.php
@@ -9,9 +9,14 @@ class ConfigServiceProvider extends ServiceProvider
 {
     public function register()
     {
+	    
+	    $cloned = $this->app->bound('config.finder') ? $this->app['config.finder'] : null;
+	    
         $this->app->singleton('config.finder', function () {
             return new ConfigFinder();
         });
+        
+        $cloned && $this->app['config.finder']->addPaths($cloned->getPaths());
 
         $this->app->singleton('config.factory', function ($container) {
             return new ConfigFactory($container['config.finder']);
@@ -26,4 +31,5 @@ class ConfigServiceProvider extends ServiceProvider
         });
         
     }
+    
 }
